176c088031cddadbaf451c10174f6c5f1883edbc
Feiko Nanninga <feiko.nanninga@rwth-aachen.de>
2015-07-07 13:15:36 +0200

    parse data for visualisation

---
+++ parse.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import datetime
+import gzip
+import json
+from pathlib import Path
+import re
+import sys
+
+def parse_aps(json_data):
+    for building in json_data:
+        for ap in building['aps']:
+            ap['building'] = building['building']
+            yield ap
+
+
+aps = {}
+roomsSpec = json.load(open('rooms.json'))
+roomFiles= {}
+for room in roomsSpec:
+    for ap in room['aps']:
+        aps[ap] = room['name']
+    parsedFilePath = 'parsed/' + room['id'] + '.json'
+    if Path(parsedFilePath).is_file():
+        pass
+    parsedData = [{'label': 'Nutzer', 'values': []}]
+    roomFiles[room['name']] = {'filename': parsedFilePath, 'file': parsedData}
+
+
+p = Path('.')
+files = list(p.glob('raw/*.json.gz'))
+files.sort()
+threshold = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=7) # ideally 7 days
+
+for f in files:
+    date_str = re.sub(
+            r"^raw/(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)([+-]\d+):(\d+).json.gz$",
+            r"\1-\2-\3T\4:\5:\6\7\8",
+            str(f))
+    date = datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S%z")
+    if (date < threshold):
+        continue
+
+    roomCounts = {}
+    for room in roomsSpec:
+        roomCounts[room['name']] = 0
+
+    decompressed = gzip.open(str(f), mode='rt')
+    parsed = json.load(decompressed)
+    for ap in parse_aps(parsed['data']):
+        if ap['name'] in aps:
+            roomCounts[aps[ap['name']]] += int(ap['user'])
+
+    for room in roomsSpec:
+        roomFiles[room['name']]['file'][0]['values'].append({
+            'time': date.isoformat(),
+            'y': roomCounts[room['name']]})
+
+    print(str(date) + ' ' + str(roomCounts))
+
+    #sys.exit()
+
+parsedDir = Path('parsed')
+if not parsedDir.is_dir():
+    parsedDir.mkdir()
+for room in roomsSpec:
+    import pprint
+    #pprint.pprint(roomFiles[room['name']]['file'])
+    json.dump(roomFiles[room['name']]['file'], open(roomFiles[room['name']]['filename'], mode='w'))
+
+
