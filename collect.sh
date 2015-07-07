7399f6d791057ea32e8f3ca6279f246554e83fd0
Feiko Nanninga <feiko.nanninga@rwth-aachen.de>
2015-07-07 14:42:35 +0200

    run parse.py on cron run

--- collect.sh
+++ collect.sh
@@ -3,7 +3,9 @@
 filename="/home/clonejo/web/rwth-wifi/`date "+%FT%H:%M:%S%:z"`.json"
 if curl -sS "http://netstatus.rz.rwth-aachen.de/wlan/map/getinfo.php?type=all" -o $filename
 then
-	gzip $filename
+    gzip $filename
+    cd /home/clonejo/web/rwth-wifi
+    ./parse.py >/dev/null
 else
-	rm -f $filename
+    rm -f $filename
 fi
